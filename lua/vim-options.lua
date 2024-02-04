a9677859f08fcc25f5541b6a33530ad5794cabe7
0xRalts <joaovvaladares1@gmail.com>
2024-02-04 11:53:09 -0300

    added bufferline + haskell lsp

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,16 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set scrolloff=10")
 vim.g.mapleader = " "
 
+-- Navigate vim panes better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.opt.termguicolors = true
