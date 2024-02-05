f18ed0b7e7d6b83281ec4caea4c753de21223da4
0xRalts <joaovvaladares1@gmail.com>
2024-02-05 12:31:41 -0300

    changed theme

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,14 +6,16 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set colorcolumn=80")
 vim.cmd("set scrolloff=10")
+
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<C-w>s", ":vsplit<CR>")
+vim.keymap.set("n", "<C-w>t", ":terminal<CR>")
 vim.opt.termguicolors = true
